e2da5eda8139260016d4618ab4e66e8abe3d74a0
r1cardo360 <ric.furlanetto@hotmail.com>
2024-10-25 19:00:18 -0300

    Criando dbcontext e adicionando models para teste

---
+++ Data/applicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Locadora_Filmes_e_Jogos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Locadora_Filmes_e_Jogos.Data
+{
+    public class applicationDbContext:DbContext
+    {
+        public applicationDbContext(DbContextOptions<applicationDbContext> option) : base(option) { }
+
+        public DbSet<Cliente> cliente { get; set; }
+
+        public DbSet<Filmes> filmes { get; set; }
+
+        public DbSet<Funcionario> funcionario { get; set; }
+
+        public DbSet<Genero> genero { get; set; }
+
+        public DbSet<Jogos> jogos { get; set; }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
+using Locadora_Filmes_e_Jogos.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<applicationDbContext>(option => option.UseSqlite(
+    builder.Configuration.GetConnectionString("DbPath")));
 
 var app = builder.Build();
 
